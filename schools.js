684adedbf4ebbb887ba74f4bdf7ae02339366ae5
BEOKALABURAGI.NORTH <badigerjaganath@gmail.com>
2026-07-19 23:49:09 +0530

    Update schools.js

--- schools.js
+++ schools.js
@@ -1,1 +1,20 @@
-
+const schools = [
+  {
+    dise: "29010100001",
+    name: "Govt HPS Tajsultanpur",
+    cluster: "Tajsultanpur",
+    type: "Government"
+  },
+  {
+    dise: "29010100002",
+    name: "Moonlight HPS & HS",
+    cluster: "Channaveer Nagar",
+    type: "Private"
+  },
+  {
+    dise: "29010100003",
+    name: "Soorya Kiran HPS",
+    cluster: "Rameshwar Nagar",
+    type: "Private"
+  }
+];
