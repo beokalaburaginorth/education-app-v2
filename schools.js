ecfb98a0ed3f0bc31276170cb7cf688822c93703
BEOKALABURAGI.NORTH <badigerjaganath@gmail.com>
2026-07-19 20:03:58 +0530

    Update schools.js

--- schools.js
+++ schools.js
@@ -1,14 +1,1 @@
-const schools = [
-{
-dise: "29030100101",
-name: "GHS KSRP Colony",
-cluster: "Tajsultanpur",
-type: "Government High School"
-},
-{
-dise: "29030100102",
-name: "Government HPS Tajsultanpur",
-cluster: "Tajsultanpur",
-type: "Government Primary School"
-}
-];
+
