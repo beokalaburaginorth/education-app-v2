b2800c2354f74d8cee319df8ba2a503e2f981cfc
BEOKALABURAGI.NORTH <badigerjaganath@gmail.com>
2026-07-30 21:36:44 +0530

    Optimize circulars sorting by removing reverse
    Remove unnecessary reverse operation on circulars array.

--- script.js
+++ script.js
@@ -754,7 +754,7 @@ function showCirculars() {
 
 let circulars = JSON.parse(localStorage.getItem("circulars")) || [];
   circulars.sort((a, b) => (b.pinned || false) - (a.pinned || false));
-circulars.reverse();
+
 let html = `
 <h2>📄 Latest Circulars</h2>
 <input type="text"
